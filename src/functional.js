2ea5ceea7da7f37b04476fe9401f247302d9729f
Ruben Shekspir <shekspir55@gmail.com>
2022-12-13 17:08:44 +0400

    frontend fixed

--- src/functional.js
+++ src/functional.js
@@ -1,64 +1,59 @@
-import {Component} from 'react';
+import { Component } from 'react';
 import raw from './text.txt'
 
-export default class Text extends Component{
-  constructor(props){
+export default class Text extends Component {
+  constructor(props) {
     super(props);
     this.state = {
       text: ""
     }
   }
-  
-  componentDidMount(){
+
+  componentDidMount() {
     let index = 0;
-    let is_back = false
-    fetch(raw)
-    .then((resp) => resp.text())
-    .then(text =>{
-      let letters = text.split("")
-      const tick = setInterval(() => {
-        
-        const check = setInterval(() => {
-          fetch(raw)
-          .then((response) => response.text())
-          .then(newText => {
-            const newLetters = newText.split("");
-            if (letters !== newLetters) {
-              letters = newLetters;
-            }
-          })
-      }, 1000)
+    let isBack = false;
+    let letters = "";
 
-
-        if (!is_back){
-          this.setState({
-              text: this.state.text + letters[index]
-          })
-          if (index + 1  === text.length){
-            is_back = true
-            index = 0
-          } else{
-            index++
+    setInterval(() => {
+      fetch("http://localhost:8080/output")
+        .then((resp) => {
+          return resp.text();
+        })
+        .then(text => {
+          const newText = JSON.parse(text).text;
+          if (newText != letters) {
+            isBack = true
           }
-        } else{
-          if (this.state.text.length > 0){
-            this.setState({
-                text: this.state.text.slice(0, -1)
-            })
-          } else{
-            is_back = false
-          }
-        }
 
-        return () => clearInterval(check)
-      }, 100)
+          letters = newText;
+        })
+    }, 10000);
 
-      
-      return () => clearInterval(tick);
-    })
+
+    setInterval(() => {
+      if (this.state.text.length === 0) {
+        isBack = false;
+      }
+
+      if (!isBack && letters.length && this.state.text.length < letters.length) {
+        this.setState({
+          text: this.state.text + letters[index]
+        });
+        return index++;
+      }
+
+      if (isBack) {
+        this.setState({
+          text: this.state.text.slice(0, -1)
+        });
+
+        return index--;
+      }
+
+    }, Math.random() * 200);
   }
 
-  render(){
+  render() {
     return (
       <div>
         {this.state.text}
